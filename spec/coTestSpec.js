87c02a8d6a1967c183cb584e34ef155f28f4441b
Vasily Nosikov <vnosikov@gmail.com>
2018-07-30 20:18:34 -0400

    quick naive refactoring

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -7,9 +7,29 @@ const Product = coTest.Product;
 describe("Co Test", function() {
 
   it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
-    const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
+    const productsAtDayZero = [
+      new Product('Medium Coverage', 10, 20),
+      new Product('Full Coverage', 2, 0),
+      new Product('Low Coverage', 5, 7),
+      new Product('Mega Coverage', 0, 80),
+      new Product('Mega Coverage', -1, 80),
+      new Product('Special Full Coverage', 15, 20),
+      new Product('Special Full Coverage', 10, 49),
+      new Product('Special Full Coverage', 5, 49),
+      new Product('Super Sale', 3, 6),
+    ];
+
+    const carInsurance = new CarInsurance(productsAtDayZero);
+    const productPrinter = function (product) {
+      console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
+    };
+
+    for (let i = 1; i <= 30; i += 1) {
+      console.log(`Day ${i}`);
+      console.log('name, sellIn, price');
+      carInsurance.updatePrice().forEach(productPrinter);
+      console.log('');
+    }
   });
 
 });
--- src/coTest.js
+++ src/coTest.js
@@ -11,55 +11,48 @@ class CarInsurance {
     this.products = products;
   }
   updatePrice() {
-    for (var i = 0; i < this.products.length; i++) {
-      if (this.products[i].name != 'Full Coverage' && this.products[i].name != 'Special Full Coverage') {
-        if (this.products[i].price > 0) {
-          if (this.products[i].name != 'Mega Coverage') {
-            this.products[i].price = this.products[i].price - 1;
-          }
-        }
-      } else {
-        if (this.products[i].price < 50) {
-          this.products[i].price = this.products[i].price + 1;
-          if (this.products[i].name == 'Special Full Coverage') {
-            if (this.products[i].sellIn < 11) {
-              if (this.products[i].price < 50) {
-                this.products[i].price = this.products[i].price + 1;
-              }
-            }
-            if (this.products[i].sellIn < 6) {
-              if (this.products[i].price < 50) {
-                this.products[i].price = this.products[i].price + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.products[i].name != 'Mega Coverage') {
-        this.products[i].sellIn = this.products[i].sellIn - 1;
-      }
-      if (this.products[i].sellIn < 0) {
-        if (this.products[i].name != 'Full Coverage') {
-          if (this.products[i].name != 'Special Full Coverage') {
-            if (this.products[i].price > 0) {
-              if (this.products[i].name != 'Mega Coverage') {
-                this.products[i].price = this.products[i].price - 1;
-              }
-            }
-          } else {
-            this.products[i].price = this.products[i].price - this.products[i].price;
-          }
-        } else {
-          if (this.products[i].price < 50) {
-            this.products[i].price = this.products[i].price + 1;
-          }
-        }
-      }
-    }
+    this.products = this.products.map(product =>
+      getUpdaterByName(product.name)(product)
+    );
 
     return this.products;
   }
 }
+
+const defaultSellInUpdater = sellIn => sellIn - 1;
+const defaultPriceUpdater = (price, sellIn) =>
+  Math.max(0, sellIn > 0 ? price - 1 : price - 2 );
+
+const fullCoveragePriceUpdater = (price, sellIn) =>
+  Math.min(50, sellIn >0 ? price + 1 : price + 2);
+
+//TODO: fixme
+const specialFullCoveragePriceUpdater = (price, sellIn) => price + 1
+
+const updater = (
+  priceUpdater = defaultPriceUpdater,
+  sellInUpdater = defaultSellInUpdater
+) => product => ({
+  price: priceUpdater(product.price, product.sellIn),
+  sellIn: sellInUpdater(product.sellIn),
+  name: product.name
+});
+
+const defaultUpdater = updater();
+const fullCoverageUpdater = updater(fullCoveragePriceUpdater);
+const megaCoverageUpdater = updater(
+  (price, sellIn) => price,
+   sellIn => sellIn
+ );
+const speicalFullCoverageUpdater = updater(specialFullCoveragePriceUpdater);
+
+const updatersDictionary = {
+  "Full Coverage": fullCoverageUpdater,
+  "Mega Coverage": megaCoverageUpdater,
+  "Special Full Coverage": specialFullCoveragePriceUpdater
+};
+
+const getUpdaterByName = name => updatersDictionary[name] || defaultUpdater;
 
 module.exports = {
   Product,
